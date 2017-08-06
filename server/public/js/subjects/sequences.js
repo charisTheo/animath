5347d9b98b199311a35078e59ebf8c6055f2847f
charisTheo <haristh@hotmail.com>
2017-08-06 16:34:41 +0300

    Fixed example1 and example2 in /sequences

--- server/public/js/subjects/sequences.js
+++ server/public/js/subjects/sequences.js
@@ -6,7 +6,7 @@ $(document).ready(()=>{
 
     function idleCheck(){
         idleTime++;
-        if (idleTime > 6 && $('body').scrollTop() < 50) {
+        if (idleTime > 8 && $('body').scrollTop() < 50) {
             $('#scrollReminder')
                 .css({
                     top: window.innerHeight - 80 + 'px'
@@ -18,28 +18,45 @@ $(document).ready(()=>{
         }
     }
 
+    $('.description').hide();
     //#intro
-    $('.description').hide().delay(1000).toggle('fold');
+    $('#intro-description').delay(1000).toggle('fold');
 
     //#example
     //#example1
     var controller = new ScrollMagic.Controller();
-    var tween = TweenMax.to('#example1', 1, {
+    var tween = new TimelineMax();
+    tween.add(TweenMax.to('#example1', 1, {
         rotation: 360, 
         scale: 1.5,
         opacity: 1,
         ease: Linear.easeNone
-    });
-    var scene = new ScrollMagic.Scene({triggerElement: '#example-1', duration: 200})
+    }));
+    tween.add(TweenMax.to('#example1-hr', 1, {
+        width: '100%', 
+        ease: Linear.easeNone,
+        onComplete: function(){$("#example1-description").toggle('bounce', {times: 3}, "slow")}
+    }));
+
+    var scene = new ScrollMagic.Scene({triggerElement: '#example-1', duration: 300})
         .setTween(tween)
         .triggerHook('onLeave')
         .setPin('#example-1', {pushFollowers: false})
         .addTo(controller);
 
-    //example2
-    var controller2 = new ScrollMagic.Controller();
-    var tween2 = TweenMax.staggerFromTo('.box', 2, {left: '100%'}, {left: 0, ease: Back.easeOut}, 0.15);
-    var scene2 = new ScrollMagic.Scene({triggerElement: '#example-2', duration: 500})
+    //#example2
+    var tween2 = new TimelineMax();
+    tween2.add(TweenMax.staggerFromTo('.box', 2, {left: '100%'}, {left: 0, scale: 1.5, ease: Back.easeOut}, 0.25));
+    tween2.add(TweenMax.to('#example2-hr', 1, {
+        width: '100%',
+        ease: Linear.easeNone,
+        onComplete: function(){$("#example2-description").toggle("bounce", {times: 3}, "slow")}
+    }));
+    var scene2 = new ScrollMagic.Scene({triggerElement: '#example-2', duration: 200, offset: 200})
         .setTween(tween2)
-        .addTo(controller2);
+        .triggerHook('onCenter')
+        .addTo(controller);
+
+    //#example3
+    
 });
