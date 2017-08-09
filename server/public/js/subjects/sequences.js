ca3e8e6da3304f2379a7df6b81bda4f50c5324cb
charisTheo <haristh@hotmail.com>
2017-08-09 15:05:13 +0300

    Added background classes & fixed scenes 2, 3, 4 in /sequences

--- server/public/js/subjects/sequences.js
+++ server/public/js/subjects/sequences.js
@@ -32,7 +32,7 @@ $(document).ready(()=>{
         opacity: 1,
         ease: Linear.easeNone
     }));
-    tween.add(TweenMax.to('#example1-hr', 2, {
+    tween.add(TweenMax.to('#example1-hr', 0.3, {
         width: '80%', 
         duration: 300,
         ease: Linear.easeNone,
@@ -42,20 +42,21 @@ $(document).ready(()=>{
     var scene = new ScrollMagic.Scene({triggerElement: '#seq-example-1', duration: 300})
         .setTween(tween)
         .triggerHook('onLeave')
-        .setPin('#seq-example-1', {pushFollowers: false})
+        .setPin('#seq-example-1', {pushFollowers: true})
         .addTo(controller);
 
     //#example2
     var tween2 = new TimelineMax();
-    tween2.add(TweenMax.staggerFromTo('.box', 2, {left: '100%'}, {left: 0, scale: 1.5, ease: Back.easeOut}, 0.25));
-    tween2.add(TweenMax.to('#example2-hr', 2, {
+    tween2.add(TweenMax.staggerFromTo('.box', 0.7, {left: '100%'}, {left: 0, scale: 1.5, ease: Back.easeOut}, 0.25));
+    tween2.add(TweenMax.to('#example2-hr', 0.3, {
         width: '80%',
         duration: 300,
         ease: Linear.easeNone,
         onComplete: function(){$("#example2-description").toggle("bounce", {times: 3}, "slow")}
     }));
-    var scene2 = new ScrollMagic.Scene({triggerElement: '#seq-example-2', duration: 200, offset: 200})
+    var scene2 = new ScrollMagic.Scene({triggerElement: '#seq-example-2', duration: 400, offset: 200})
         .setTween(tween2)
+        .setPin('#seq-example-2', {pushFollowers: true})
         .triggerHook('onCenter')
         .addTo(controller);
 
@@ -65,22 +66,40 @@ $(document).ready(()=>{
     $path.css("stroke-dashoffset", lineLength);
 
     //#example3
-    var tween3 = TweenMax.to($path, 1, {strokeDashoffset: 0, ease:Linear.easeNone, onComplete: function(){
-        $('#example3').slideDown('slow');
-    }});
+    var tween3 = new TimelineMax();
+    tween3.add(
+        TweenMax.to($path, 1.5, {strokeDashoffset: 0, ease:Linear.easeNone, onComplete: function(){
+            $('#example3').slideDown('slow');
+        }})
+    );
+    tween3.add(
+        TweenMax.to($path, 0.1, {strokeDasharray: '11, 22', ease:Linear.easeNone})
+    );
     var scene3 = new ScrollMagic.Scene({triggerElement: '#svg-line-section', duration: 300})
         .setTween(tween3)
         .triggerHook('onLeave')
         .addTo(controller);
 
     //#example4        
-    var tween4 = TweenMax.to('#eight', 0.5, {scale: 1.5, repeat: 4, yoyo: true, duration: 1000, onComplete: function(){
-        $('#example4').toggle('pulsate', {times: 3}); 
-        $('#example4-description').delay(1000).toggle('bounce', {times: 3});                
+    var tween3_4 = TweenMax.fromTo('#eight', 2, {scale: 0}, {scale: 2, opacity: 0, repeat: 4, yoyo: true, duration: 1000, onComplete: function(){
+        $('#example4').hide().toggle('pulsate', {times: 3});
     }});
-    var scene4 = new ScrollMagic.Scene({triggerElement: '#seq-example-3', duration: 300})
-        .setTween(tween4)
+    var scene3_4 = new ScrollMagic.Scene({triggerElement: '#seq-example-3', duration: 500})
+        .setTween(tween3_4)
+        .setPin('#eight', {pushFollowers: true})
         .triggerHook('onLeave')        
         .addTo(controller);
-    
+
+    var tween4 = TweenMax.to('#example4-hr', 0.3, {
+        width: '80%',
+        duration: 300,
+        ease: Linear.easeNone,
+        onComplete: function(){$('#example4-description').hide().toggle('bounce', {times: 3})}
+    });
+    var scene4 = new ScrollMagic.Scene({triggerElement: '#seq-example-4', duration: 400})
+        .setTween(tween4)
+        .setPin('#seq-example-4', {pushFollowers: true})
+        .triggerHook('onCenter')
+        .addTo(controller);
+
 });
