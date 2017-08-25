617ef8dfbe516d0d2c9211183d9d1ed0c2df5525
charisTheo <haristh@hotmail.com>
2017-08-25 20:33:03 +0300

    example2 in /limits further explained

--- server/public/js/subjects/limits.js
+++ server/public/js/subjects/limits.js
@@ -16,23 +16,27 @@ $(document).ready(function() {
 
     //example2
     var $frame = $('rect#example2-frame')
+        $arrow = $('path.arrow')
         frameWidth = $('#example2').innerWidth()
         frameHeight = $('#example2').innerHeight();
     $frame.css({
         width: `${frameWidth}px`,
         height: `${frameHeight}px` 
     });
+    configSVG($arrow);
     configSVG($frame);
-    $('#distanceSVG')
-        .width('100%')
-        .css('padding', '20%')
-        .hide();
+    $('#distanceSVG').hide();
+    $('#arrowSVG').hide();
+    $('#example2-arrow-ex').hide();
+
     function checkDistance() {
         //show distance covered in 3 d.p. 
-        if (this.progress().toFixed(3) > 0.973) {
-            sequenceTerm.textContent = 0.999;
+        var distanceCovered = this.progress().toFixed(3);
+        if (distanceCovered >= 0.968) {
+            return false;
         } else {
-            sequenceTerm.textContent = this.progress().toFixed(3);
+            sequenceTerm.textContent = distanceCovered;
+            $('#difference').text(Number(1 - distanceCovered).toFixed(3));
         }
     };
 
@@ -41,7 +45,7 @@ $(document).ready(function() {
         .to('rect#example2-frame', 1, {strokeDashoffset: 0}, 0)
         .to('#example2', 0.5, {
                 scale: 0.7,
-                top: '-20vh',
+                top: '-10vh',
                 onComplete: function(){
                     if (!$('#distanceSVG').is(':visible')) {
                         $('#distanceSVG').show('pulsate', 'slow');
@@ -51,14 +55,25 @@ $(document).ready(function() {
         .add('scale', 4.4)
         .add('end', 2)
         .to('#distanceRect', 4, {
-            width: 232, 
+            width: 222, 
             left: '10vw', 
+            ease: Linear.easeNone,
+            onUpdate: function(){
+                if (this.progress() >= 0.968) false;
+            },
+        }, 'end')
+        .to('#differenceLine', 4, {
+            scaleX: 0.025,
+            transformOrigin: '100% top', 
             ease: Linear.easeNone
         }, 'end')
         .staggerTo('.distance', 4, {
-            x: 230,
-            ease: Linear.easeNone,
-            onUpdate: checkDistance
+            x: 220,
+            ease: Linear.easeOut,
+            onUpdate: checkDistance,
+            onComplete: function(){
+                $('#arrowSVG').show('fade');
+            }
         }, 0, 'end')
         .to('#sequenceTerm', 0.5, {
             scale: 0.5,
@@ -67,14 +82,22 @@ $(document).ready(function() {
         }, 'scale')
         .to('#distanceSVG', 0.5, {
             scale: 2.5,
-            transformOrigin: '100% 50%',
+            transformOrigin: '80% 50%',
             ease: Linear.easeOut
-        }, 'scale'); 
+        }, 'scale')
+        .staggerTo('path.arrow', 1, {
+            strokeDashoffset: 0, 
+            ease: Linear.easeOut,
+            onComplete: function(){
+                if (!$('#example2-arrow-ex').is(':visible')) {
+                    $('#example2-arrow-ex').show('fade', 'slow');
+                }
+            }
+        }, 0.5, 'end+=2');
         
-    var scene2 = new ScrollMagic.Scene({triggerElement: '#lim-example-2', duration: '300%'})
+    var scene2 = new ScrollMagic.Scene({triggerElement: '#lim-example-2', duration: '400%'})
         .setTween(tween2)
         .setPin('#lim-example-2', {pushFollowers: true})
         .triggerHook('onLeave')
         .addTo(controller);
-
 });
