425c246c76944f0534c6125a3f28b00425cf7f5e
charisTheo <haristh@hotmail.com>
2017-09-04 20:32:34 +0300

    Staggered statements and properties in example3 in /limits

--- server/public/js/subjects/limits.js
+++ server/public/js/subjects/limits.js
@@ -169,20 +169,35 @@ $(document).ready(function() {
         .addTo(controller);
 
     //example3
-    $('#example3-description').show();
     var tween3 = new TimelineMax()
-        .fromTo('#example3', 1, {opacity: 0}, {opacity: 1, ease: SlowMo.ease.config(0.7, 0.7, false)});
-        // .staggerFromTo('.lim-stagger', 4, {scale: 3}, {scale: 1, ease: Power4.easeOut});
+        .fromTo('#example3', 1, {opacity: 0, scale: 0.2}, {opacity: 1, scale: 1.3, y: '-100px', rotation: 360, ease: Linear.easeOut})
+        .add(TweenMax.staggerFromTo('.lim-stagger', 4, {opacity: 0, scale: 3}, {opacity: 1, scale: 1, ease: Power4.easeOut}, 0.5,
+            // on completeAll function
+            function(){
+                var tween3_1 = new TimelineMax();
+                tween3_1.add(TweenMax.staggerTo('.lim-stagger', 1, {
+                    y: '-80px',
+                    transformOrigin: "top middle",
+                    ease: Bounce.easeOut
+                }, 0.3, 0));
+                tween3_1.play();
+            }
+        ))
+        .add(TweenMax.staggerFromTo('h4.property', 2, {
+            scale: 0.3, 
+            opacity: 0,
+            x: '80%'
+        }, {
+            scale: 1, 
+            opacity: 1,
+            x: '0%',
+            ease: Back.easeOut.config(1.7)
+        }, 4));
 
-    //add offset
-    var scene3 = new ScrollMagic.Scene({triggerElement: '#lim-example-3', duration: '200%'})
+    var scene3 = new ScrollMagic.Scene({triggerElement: '#lim-example-3', duration: '400%'})
         .setTween(tween3)
         .setPin('#lim-example-3', {pushFollowers: true})
         .triggerHook('onLeave')
         .addTo(controller);
 
-    var scene3_1 = new ScrollMagic.Scene({triggerElement: '#lim-example-3'})
-        .setTween('#example3', 0.5, {scaleY: 1, opacity: 1})
-        .triggerHook('onLeave')
-        .addTo(controller);
 });
